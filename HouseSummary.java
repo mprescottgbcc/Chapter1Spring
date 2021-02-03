00f57b7d728639dd0e9e54c8929b271c19e6ac27
Kelsey Pelletier <78060782+KelpCodes@users.noreply.github.com>
2021-02-02 21:06:44 -0500

    Add files via upload

---
+++ HouseSummary.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HouseSummary {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int currentPrice; 
+      int lastMonthsPrice; 
+      
+      currentPrice = scnr.nextInt(); 
+      lastMonthsPrice = scnr.nextInt();
+      
+      
+      I honestly have been wracking my brain about what equation I need to communicate the consistant change within the change of
+      Price. I'm happy the mortgage algorithm was provided because I feel like im so close but. I have to call it here.
+      
+      System.out.print("This house is $" + currentPrice + ".");
+      System.out.println(" The change is $" + (lastMonthsPrice + " since last month.");
+      System.out.println("The estimated monthly mortgage is $" + (currentPrice * 0.051 / 12) + ".");
+   }
+}
---
+++ RightArrow.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+
+public class RightArrow {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in); 
+      int baseChar; 
+      int headChar; 
+      
+      System.out.println("     1");
+      System.out.println("     11");
+      System.out.println("00000111");
+      System.out.println("000001111");
+      System.out.println("00000111");
+      System.out.println("     11");
+      System.out.println("     1");
+      /* Type your code here. */
+   }
+}
